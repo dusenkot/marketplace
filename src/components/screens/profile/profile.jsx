2c8c62c3b7cf506118d5abeabec6cd70a4ec5db0
dusenkot <dusenkot@gmail.com>
2023-05-20 23:27:54 +0200

    fix profile

--- src/components/screens/profile/profile.jsx
+++ src/components/screens/profile/profile.jsx
@@ -1,50 +1,79 @@
-import React from 'react';
-import style from './profile.css'
-import image from  './pic.png'
-import image2 from  './startuper.jpg'
-function Bound(){
-return <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg"></input>
+import React, { useRef } from 'react';
+import style from './profile.css';
+import image from './pic.png';
+import image2 from './startuper.jpg';
+
+function Bound() {
+  const inputRef = useRef(null);
+
+  const handleFileSelect = () => {
+    inputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    console.log('You selected the file:', file);
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        id="avatar"
+        name="avatar"
+        accept="image/png, image/jpeg"
+        style={{ display: 'none' }}
+        ref={inputRef}
+        onChange={handleFileChange}
+      />
+      <a href="#" onClick={handleFileSelect}>
+        Choose avatar picture
+      </a>
+    </div>
+  );
 }
 
-function Profile(){
-    return(
-        <div >
-            <div>
-                <div id="wsp">
-                    <img src={image2} style={style.img} id="prof"/>
-                        <img src={image} style={style.img} id="back"/>
-                        <p id="profil">Profile of start-upper</p>
-                    
-                
-            </div>
+function Profile() {
+  return (
+    <div>
+      <div>
+        <div id="wsp">
+          <img src={image2} style={style.img} id="prof" />
+          <img src={image} style={style.img} id="back" />
+          <p id="profil">Profile of start-upper</p>
+        </div>
         <div id="pl" style={style.pl}>
-            <h4 id="p">Wojciech Nowak</h4>
-            <ul>
-                 <li><a href="" onClick={Bound}>Choose avatar picture</a></li>
-                 <li><a href="">Contact with me</a></li>
-                 <li><a href="" >Your wallet</a></li>
-             </ul>
-            
-
-            
+          <h4 id="p">Wojciech Nowak</h4>
+          <ul>
+            <li>
+              <Bound />
+            </li>
+            <li>
+              <a href="#">Contact with me</a>
+            </li>
+            <li>
+              <a href="#">Your wallet</a>
+            </li>
+          </ul>
         </div>
         <div id="pr" style={style.pr}>
-        <h2 id="inf">Personal information</h2>
-            <form>
-                <input type="text" id="text" name="text" value="Tell something about yourself"/>
-            </form>
-            <h2>Description of the start-up</h2>
-            <form>
-                <input type="text" id="text" name="text" value="Please, don`t discribe your project in details. wait until the investor contracts with you"/>
-            </form>
-
+          <h2 id="inf">Personal information</h2>
+          <form>
+            <input type="text" id="text" name="text" defaultValue="Tell something about yourself" />
+          </form>
+          <h2>Description of the start-up</h2>
+          <form>
+            <input
+              type="text"
+              id="text"
+              name="text"
+              defaultValue="Please, don't describe your project in detail. Wait until the investor contracts with you"
+            />
+          </form>
         </div>
+      </div>
     </div>
-
-        </div>
-
-    )
+  );
 }
 
-
-export default Profile
+export default Profile;
