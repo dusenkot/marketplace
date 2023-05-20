f4f5aff89a7efbb0d3be7c63198fa15b32c6fe00
w67971 <mvolokhonskaya@gmail.com>
2023-05-20 18:36:53 +0300

    1

--- src/components/screens/profile/profile.jsx
+++ src/components/screens/profile/profile.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import './App.css';
+import style from './profile.css'
 
 function Profile(){
     return(
-        <div>
-            <h1>Hej</h1>
+        <div className={style.body} >
+            <div>
+        <h1>Profile</h1>
+        <div id="pl">
+            <img src="default.jpg"/>
+            <form >
+                <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg"/>
+            </form>
+            
+            <h4 id="p">Name and Surname</h4>
+        </div>
+        <div id="pr">
+            <h2>Description of the start-up</h2>
+            <form>
+                <input type="text" id="text" name="text" value="You can enter description here"/>
+            </form>
+
+        </div>
+    </div>
+
         </div>
 
     )
