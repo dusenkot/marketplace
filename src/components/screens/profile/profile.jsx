e61f038527b4b31e610b44059507fa9f8b26bcbf
= <=>
2023-05-21 01:12:03 +0200

    upgraded styles on page profile.jsx

--- src/components/screens/profile/profile.jsx
+++ src/components/screens/profile/profile.jsx
@@ -19,7 +19,7 @@ function Bound() {
 
   return (
     <div>
-        <App />
+        
       <input
         type="file"
         id="avatar"
@@ -38,12 +38,15 @@ function Bound() {
 
 function Profile() {
   return (
-    <div>
-      <div>
+    <div style={styles.background}>
+      <App />
+      <div >
         <div id="wsp">
           <img src={image2} style={style.img} id="prof" />
           <img src={image} style={style.img} id="back" />
           <p id="profil">Profile of start-upper</p>
+
+          
         </div>
         <div id="pl" style={style.pl}>
           <h4 id="p">Wojciech Nowak</h4>
@@ -59,6 +62,11 @@ function Profile() {
             </li>
           </ul>
         </div>
+
+
+
+
+
         <div id="pr" style={style.pr}>
           <h2 id="inf">Personal information</h2>
           <form>
@@ -79,4 +87,10 @@ function Profile() {
   );
 }
 
+const styles = {
+  background: {
+    background: 'linear-gradient(to bottom right, #005c98, #005c98 0%, #003261 0%, #005c98 75%)',
+    minHeight: '100vh',
+  },
+}
 export default Profile;
