2a1d159baa604692fb7bbf8c110d9800900d6552
dusenkot <dusenkot@gmail.com>
2023-05-20 13:47:16 +0200

    1.6 działa

--- src/components/sceens/home/Home.jsx
+++ src/components/sceens/home/Home.jsx
@@ -4,13 +4,7 @@ import style from './Home.module.css'
 function Home() {
 
   return (
-  <div>
-    <div class="navbar">  
-    <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a>
-    <a href="#"><i class="fa fa-fw fa-search"></i> Search</a>
-    <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact</a>
-    <a href="#"><i class="fa fa-fw fa-user"></i> Login</a>
-  </div>
+    <div>
     <h1>Cars catalog</h1>
     <div>
         <div className={style.item}>
