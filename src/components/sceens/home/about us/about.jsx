4ced84c71cb3cd7d1563bc0ca62e0c5c1611fdfd
= <=>
2023-05-20 15:26:40 +0300

    Siamons upgrade

--- src/components/sceens/home/about us/about.jsx
+++ src/components/sceens/home/about us/about.jsx
@@ -1,7 +1,32 @@
 function About(){
+    const title1 = {
+        fontSize : "100px",
+    }
+    const title2 = {
+        margin:"15px",
+        paddingLeft: "100px",
+        fontSize: "80px"
+    }
+    const body = {
+        backgroundColor:"blue"
+    }
+    const chooseus = {
+        fontSize:"70px",
+        paddingLeft: "25px"
+    }
+    const choosseus1 = {
+        paddingLeft:"35px"
+    }
+    const our_users = {
+        paddingLeft:"47%",
+        fontSize: ""
+    }
     return(
-        <div>
-            <h1>Staro mordo</h1>
+        <div style = {body}>
+            <h1 class="title1" style = {title1}>Your future - </h1><h2 style = {title2}>starts here</h2>
+    <h3 style = {chooseus}>Why should you choose us?</h3>
+    <p style = {chooseus1}>user-friendly interface, convenience and security, smart communication, quality feedback, development and improvment of the platform</p>
+    <h2 style = {our_users}>Our users and their proposals</h2>
         </div>
 
     )
