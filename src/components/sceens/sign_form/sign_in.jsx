4f201a55e24be7b775b8b503aa744c916f0c2c65
= <=>
2023-05-20 17:14:24 +0200

    Hueta Pavla

---
+++ src/components/sceens/sign_form/sign_in.jsx
@@ -0,0 +1,63 @@
+import style from './sign_in.css'
+
+function sing_in() {
+
+    return (
+        <div style={styles.container}>
+          <h2 style={styles.heading}>Sign In</h2>
+          <form style={styles.form}>
+            <label htmlFor="email" style={styles.label}>
+              Email:
+            </label>
+            <input type="email" id="email" name="email" required style={styles.input} />
+    
+            <label htmlFor="password" style={styles.label}>
+              Password:
+            </label>
+            <input type="password" id="password" name="password" required style={styles.input} />
+    
+            <input type="submit" value="Sign In" style={styles.submitButton} />
+          </form>
+        </div>
+      );
+    };
+    
+    const styles = {
+      container: {
+        backgroundColor: '#263238',
+        color: '#ffffff',
+        padding: '20px',
+        borderRadius: '5px',
+        maxWidth: '400px',
+        margin: '0 auto',
+      },
+      heading: {
+        textAlign: 'center',
+        marginBottom: '20px',
+      },
+      form: {
+        display: 'flex',
+        flexDirection: 'column',
+      },
+      label: {
+        marginBottom: '8px',
+      },
+      input: {
+        padding: '10px',
+        border: '1px solid #78909c',
+        borderRadius: '3px',
+        marginBottom: '15px',
+        color: '#ffffff',
+        backgroundColor: '#37474f',
+      },
+      submitButton: {
+        backgroundColor: '#1976d2',
+        color: '#ffffff',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '3px',
+        cursor: 'pointer',
+      },
+    };
+    
+    export default SignInWindow;
---
+++ src/components/sceens/sign_form/sign_up.jsx
@@ -0,0 +1,28 @@
+import style from './sign_up.css'
+
+function sing_up() {
+
+  return (
+    
+    <form style={styles.form}>
+      <label htmlFor="name" style={styles.label}>Name:</label>
+      <input type="text" id="name" name="name" required style={styles.input} />
+
+      <label htmlFor="surname" style={styles.label}>Surname:</label>
+      <input type="text" id="surname" name="surname" required style={styles.input} />
+
+      <label htmlFor="login" style={styles.label}>Login:</label>
+      <input type="text" id="login" name="login" required style={styles.input} />
+
+      <label htmlFor="email" style={styles.label}>Email:</label>
+      <input type="email" id="email" name="email" required style={styles.input} />
+
+      <label htmlFor="password" style={styles.label}>Password:</label>
+      <input type="password" id="password" name="password" required style={styles.input} />
+
+      <input type="submit" value="Submit" style={styles.submitButton} />
+    </form>
+  );
+};
+
+export default sing_up
