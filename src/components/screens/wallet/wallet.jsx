afedd33b38c9937939bf766e74dfdb810cc282ec
= <=>
2023-05-21 02:49:03 +0300

    Wallet

--- src/components/screens/wallet/wallet.jsx
+++ src/components/screens/wallet/wallet.jsx
@@ -1,5 +1,3 @@
-// paymentWrapper.jsx
-
 import React from 'react';
 
 class PaymentWrapper extends React.Component {
@@ -21,7 +19,7 @@ class PaymentWrapper extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    // Місце для логіки обробки платежу
+    // Place your payment processing logic here
     console.log('Payment submitted!');
     console.log('Amount:', this.state.paymentAmount);
     console.log('Method:', this.state.paymentMethod);
@@ -29,32 +27,83 @@ class PaymentWrapper extends React.Component {
 
   render() {
     return (
-      <div>
-        <h2>Payment System</h2>
+      <div style={styles.main}>
+        <h2 style={styles.heading}>Payment System</h2>
         <form onSubmit={this.handleSubmit}>
-          <label>
+          <label style={styles.label}>
             Payment Amount:
             <input
               type="number"
               value={this.state.paymentAmount}
               onChange={this.handleAmountChange}
+              style={styles.input}
             />
           </label>
           <br />
-          <label>
+          <label style={styles.label}>
             Payment Method:
-            <select value={this.state.paymentMethod} onChange={this.handleMethodChange}>
+            <select
+              value={this.state.paymentMethod}
+              onChange={this.handleMethodChange}
+              style={styles.select}
+            >
               <option value="creditCard">Credit Card</option>
               <option value="paypal">PayPal</option>
               <option value="bankTransfer">Bank Transfer</option>
             </select>
           </label>
           <br />
-          <button type="submit">Submit Payment</button>
+          <button type="submit" style={styles.button}>Submit Payment</button>
         </form>
       </div>
     );
   }
 }
+
+const styles = {
+  main: {
+    textAlign: 'center',
+    background: '#4040ff',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading: {
+    color: '#fff',
+    fontSize: '24px',
+    fontWeight: 'bold',
+    marginBottom: '20px',
+  },
+  label: {
+    color: '#fff',
+    fontSize: '18px',
+    marginBottom: '10px',
+  },
+  input: {
+    padding: '5px',
+    borderRadius: '4px',
+    border: 'none',
+    marginBottom: '10px',
+  },
+  select: {
+    padding: '5px',
+    borderRadius: '4px',
+    border: 'none',
+    marginBottom: '10px',
+  },
+  button: {
+    padding: '10px 20px',
+    borderRadius: '4px',
+    border: 'none',
+    background: '#00ff00',
+    color: '#fff',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+    marginBottom: '10px',
+  },
+};
 
 export default PaymentWrapper;
