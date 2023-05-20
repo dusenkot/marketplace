703cf8bf8c2d81890694f832661c8854b3a02538
dusenkot <dusenkot@gmail.com>
2023-05-20 17:55:12 +0200

    fix123

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import App from './form-send/forms-sends';
 import style from './Home.module.css'
 
 
@@ -9,7 +8,6 @@ function Home() {
   <div className={style.item}>
     <div><h1>Profite Pulse</h1></div>
     <div><h1>Menu</h1></div>
-
   </div>
   )
 }
