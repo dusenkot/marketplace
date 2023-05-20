b4a2520a61a55dbaa8f57d6ddc887cc7937ca788
w67971 <mvolokhonskaya@gmail.com>
2023-05-20 22:59:11 +0300

    1

--- src/components/screens/profile/profile.jsx
+++ src/components/screens/profile/profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from './profile.css'
 import image from  './pic.png'
-import image2 from  './default.jpg'
+import image2 from  './startuper.jpg'
 
 function Profile(){
     return(
@@ -10,23 +10,29 @@ function Profile(){
                 <div id="wsp">
                     <div id="yor">The start-uppers profile</div>
                     <img src={image2} style={style.img} id="prof"/>
-                    <img src={image} style={style.img} id="back"/>
+                    <div>
+                        <img src={image} style={style.img} id="back"/>
+                        <p>Profile of start-upper</p>
+                    </div>
+                    
                 
             </div>
         <div id="pl" style={style.pl}>
-            <h4 id="p">Name and Surname</h4>
-            <form >
-                <input type="file"  id="avatar"  name="avatar" accept="image/png, image/jpeg"/>
-               
-            </form>
-            
-            
-            <button>Contact with me</button>
+            <h4 id="p">Wojciech Nowak</h4>
+            <ul>
+                 <li><a href="">Choose avatar picture</a></li>
+                 <li><a href="">Contact with me</a></li>
+                 <li><a href="">Your wallet</a></li>
+            </ul>
         </div>
         <div id="pr" style={style.pr}>
+        <h2>Personal information</h2>
+            <form>
+                <input type="text" id="text" name="text" value="Tell something about yourself"/>
+            </form>
             <h2>Description of the start-up</h2>
             <form>
-                <input type="text" id="text" name="text" value="You can enter description here"/>
+                <input type="text" id="text" name="text" value="Please, don`t discribe your project in details. wait until the investor contracts with you"/>
             </form>
 
         </div>
